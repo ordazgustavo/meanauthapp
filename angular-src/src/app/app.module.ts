7e5b556382920e65594004a69cc4e38ebfea50ac
Gustavo Ordaz <ordazsgustavo@gmail.com>
2017-06-24 02:11:15 -0400

    App ready

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,19 +2,51 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { ValidateService } from './validate.service';
+import { AuthService } from './auth.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes = [
+   {path: '', component: HomeComponent },
+   {path: 'register', component: RegisterComponent },
+   {path: 'login', component: LoginComponent },
+   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [
+     ValidateService,
+     AuthService,
+     AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- routes/users.js
+++ routes/users.js
@@ -16,9 +16,9 @@ router.post('/register', (req, res, next) => {
 
    User.addUser(newUser, (err, user) => {
       if (err) {
-         res.json({success: false, mes: 'Failed to register user'});
+         res.json({success: false, msg: 'Failed to register user'});
       } else {
-         res.json({success: true, mes: 'User registered'});
+         res.json({success: true, msg: 'User registered'});
       }
    });
 });
